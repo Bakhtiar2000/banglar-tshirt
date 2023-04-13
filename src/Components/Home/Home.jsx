68404cac414984a6f455edc0ffcac7bd7b58feb6
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-04-13 23:38:53 +0600

    added button

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css'
 import { useLoaderData } from 'react-router-dom';
+import Tshirt from '../Tshirt/Tshirt';
+import Cart from '../Cart/Cart';
 
 
 const Home = () => {
     const tshirts = useLoaderData()
+    const [cart,setCart]= useState([])
+
+    const handleAddToCart= tshirt=>{
+        console.log(tshirt)
+    }
     return (
-        <div>
-            <h2>This is home:{tshirts.length}</h2>
+        <div className='home-container'>
+            <div className='tshirts-container'>
+                {
+                    tshirts.map(tshirt=> <Tshirt              
+                        key={tshirt._id}
+                        tshirt={tshirt}    
+                        handleAddToCart={handleAddToCart}            
+                    ></Tshirt>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
